7210cee7d459efd41369ea162b6d425c28a296c5
jiyy25 <jiyy825@gmail.com>
2024-06-09 00:10:57 +0900

    데이터 db연결

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,18 +1,18 @@
-import video from "../models/video";
+import Video from "../models/video";
 
 export const handleHome = async (req, res) =>{
-    const videos = await video.find({})
-    return res.render("home",{pageTitle: "home", videos:[]});
+    const videos = await Video.find({})
+    return res.render("home",{pageTitle: "home", videos});
 }  //res.render("view") , express가 views 디렉토리에서 그 이름의 pug파일을 찾음, render는 퍼그파일의 이름을 전해주는 것
 
 export const watch = (req, res) => {
     const { id } = req.params; //주소의 id값
-    return res.render("watch",{pageTitle: `Watching ${video.title}`});
+    return res.render("watch",{pageTitle: `Watching ${Video.title}`});
 } //base.pug에서 #{변수} => 각각의 컨트롤러에서 변수값을 보내 줄 수 있음
 
 export const getEdit = (req, res) =>{
     const { id } = req.params;
-    return res.render("edit",{pageTitle: `Edit ${video.title}`});
+    return res.render("edit",{pageTitle: `Edit ${Video.title}`});
 } 
 
 export const postEdit = (req, res) =>{
@@ -26,8 +26,17 @@ export const getUploadVideo = (req, res) =>{
     return res.render("upload", {pageTitle: `upload video`});
 };
 
-export const postUploadVideo = (req, res) => {
-    // console.log("upload:::::",req.body)
-    const {title} = req.body;
+export const postUploadVideo = async(req, res) => {
+    const {title, description, createAt, hashtags, meta } = req.body;
+    await Video.create({
+        title,
+        description,
+        createAt : Date.now(),
+        hashtags: hashtags.split(",").map(hash=> `#${hash}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+    })
     return res.redirect("/");
-}
+};
--- src/models/video.js
+++ src/models/video.js
@@ -13,6 +13,6 @@ const videoSchema = new mongoose.Schema({
 //데이터베이스에게 보내줄 데이터의 값이 어떤 타입인지 알려줘야함. 
 //스키마는 데이터 베이스를 매핑하는데 사용되는 정의 정보.
 
-const video = mongoose.model("Video", videoSchema); //일반적으로 몽구스 모델의 이름에는 대문자 첫 글자를 사용
-export default video;
+const Video = mongoose.model("Video", videoSchema); //일반적으로 몽구스 모델의 이름에는 대문자 첫 글자를 사용
+export default Video;
 
